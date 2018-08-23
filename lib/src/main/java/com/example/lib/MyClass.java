85d5ab83492920869ca36fc76d4537167ca2e948
chenggongzhao <gongzhao.cheng@rayjoy.com>
2018-08-23 12:21:55 +0800

    更改器方法，访问器方法

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+import java.lang.reflect.Field;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+public class MyClass {
+	 static public void main(String[] args){
+		 JFrame frame = new JFrame();
+//		 JTable table = new JTable();
+//		 frame.add(pane);
+		 frame.pack();
+		 frame.setVisible(true);
+
+	}
+}
---
+++ lib/src/main/java/com/example/lib/PrimitiveTypeTypeTest.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+public class PrimitiveTypeTypeTest {
+	public static void main(String [] args){
+		//byte
+		System.out.println("基本类型：byte 二进制位数"+Byte.SIZE);
+		System.out.println("包装类：java.lang.Byte");
+		System.out.println("最小值：Byte.MIN_VALUE="+Byte.MIN_VALUE);
+		System.out.println("最大值：Byte.MAX_VALUE="+Byte.MAX_VALUE);
+		System.out.println();
+
+
+
+	}
+}
